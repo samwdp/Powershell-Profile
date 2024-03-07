193d3e6a5e846fa6fa7d42d06ebdc2cf2f90e0a2
Sam Precious <samwdp@gmail.com>
2024-03-07 09:44:29 +0000

    feat: some keybinds

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,105 +1,44 @@
-Import-Module Get-ChildItemColor
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
 
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding =
-                    New-Object System.Text.UTF8Encoding
-$global:forePromptColor = 0
-$global:leftArrow = [char]0xe0b2
-$global:rightArrow = [char]0xe0b0
-$global:esc = "$([char]27)"
-$global:fore = "$esc[38;5"
-$global:back = "$esc[48;5"
-$global:prompt = ''
-$global:name = Translate-ToRunes "sam" $true
+Set-PSReadLineOption -BellStyle None
 
-[System.Collections.Generic.List[ScriptBlock]]$global:PromptRight = @(
-    # right aligned
-    { "$fore;${errorColor}m{0}" -f $leftArrow }
-    { "$fore;${forePromptColor}m$back;${errorColor}m{0}" -f $(if (@(get-history).Count -gt 0) {(get-history)[-1] | % { "{0:c}" -f (new-timespan $_.StartExecutionTime $_.EndExecutionTime)}}else {'00:00:00.0000000'}) }
-
-    { "$fore;8m$back;${errorColor}m{0}" -f $leftArrow }
-    { "$fore;0m$back;8m{0}" -f $(get-date -format "hh:mm:ss tt") }
-)
-
-[System.Collections.Generic.List[ScriptBlock]]$global:PromptLeft = @(
-    # left aligned
-    { "$fore;${forePromptColor}m$back;${global:platformColor}m{0}" -f $('{0:d4}' -f $name) }
-    { "$back;22m$fore;${global:platformColor}m{0}" -f $rightArrow }
-
-    { "$back;22m$fore;${forePromptColor}m{0}" -f $(if ($pushd = (Get-Location -Stack).count) { "$([char]187)" + $pushd }) }
-    { "$fore;22m$back;5m{0}" -f $rightArrow }
-
-    { "$back;5m$fore;${forePromptColor}m{0}" -f $($pwd.Drive.Name) }
-    { "$back;14m$fore;5m{0}" -f $rightArrow }
-
-    { "$back;14m$fore;${forePromptColor}m{0}$esc[0m" -f $(Split-Path $pwd -leaf) }
-)
-
-function global:prompt {
-    $global:errorColor = if ($?) {22} else {1}
-    $global:platformColor = if ($isWindows) {11} else {117}
-
-    $gitTest = $(git config -l) -match 'branch\.'
-    if (-not [string]::IsNullOrEmpty($gitTest)) {
-        $branch = git symbolic-ref --short -q HEAD
-        $aheadbehind = git status -sb
-        $distance = ''
-
-        if (-not [string]::IsNullOrEmpty($(git diff --staged))) { $branchbg = 3 }
-        else { $branchbg = 5 }
-
-        if (-not [string]::IsNullOrEmpty($(git status -s))) { $arrowfg = 3 }
-        else { $arrowfg = 5 }
-
-        if ($aheadbehind -match '\[\w+.*\w+\]$') {
-            $ahead = [regex]::matches($aheadbehind, '(?<=ahead\s)\d+').value
-            $behind = [regex]::matches($aheadbehind, '(?<=behind\s)\d+').value
-
-            $distance = "$back;15m$fore;${arrowfg}m{0}$esc[0m" -f $rightArrow
-            if ($ahead) {$distance += "$back;15m$fore;${forePromptColor}m{0}$esc[0m" -f "a$ahead"}
-            if ($behind) {$distance += "$back;15m$fore;${forePromptColor}m{0}$esc[0m" -f "b$behind"}
-            #$distance += "$fore;15m{0}$esc[0m" -f $rightArrow
-        }
-        else {
-            $distance = "$fore;${arrowfg}m{0}$esc[0m" -f $rightArrow
-        }
-
-        [System.Collections.Generic.List[ScriptBlock]]$gitPrompt = @(
-            { "$back;${branchbg}m$fore;14m{0}$esc[0m" -f $rightArrow }
-            { "$back;${branchbg}m$fore;${forePromptColor}m{0}$esc[0m" -f $branch }
-            { "{0}$esc[0m" -f $distance }
-        )
-        $leftSide = -join @($global:PromptLeft + $gitPrompt + {" "}).Invoke()
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
-    else {
-        $leftSide = -join @($global:PromptLeft + { "$fore;14m{0}$esc[0m" -f $rightArrow } + {" "}).Invoke()
+}
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
-
-    $rightSide = -join ($global:promptRight).Invoke()
-    $offset = $global:host.UI.RawUI.BufferSize.Width - 28
-    $prompt = -join @($leftSide, "$esc[${offset}G", $rightSide, "$esc[0m" + "`n`r`>_ ")
-    $prompt
 }
 
-function fuzzy-f
+function YoutubeMusic
 {
-    Get-ChildItem . -Recurse | ? { -not $_.PSIsContainer } | Invoke-Fzf | % { runemacs  $_ }
+    & "C:\Program Files\BraveSoftware\Brave-Browser\Application\chrome_proxy.exe" --profile-directory=Default --app-id=cinhimbnkkaeohfgghhklpknlkffjgod  #--profile-directory=Default --app-id=cinhimbnkkaeohfgghhklpknlkffjgod
 }
 
-function fuzzy-cd
-{
-    Get-ChildItem . -Recurse | ? { $_.PSIsContainer } | Invoke-Fzf | Set-Location
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Vi
+
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PSReadLineKeyHandler -Key Ctrl+m -ScriptBlock { YoutubeMusic }
+Set-PSReadLineKeyHandler -Chord Ctrl+f -ViMode Insert -ScriptBlock {
+    Set-Location -Path (Get-ChildItem -Path @("d:\work", "d:\projects") -Directory | ForEach-Object { $_.FullName } | Invoke-Fzf)
 }
 
-Set-Alias ff fuzzy-f
+Set-Alias -Name ytm -Value YoutubeMusic
+Set-Alias -Name vim -Value nvim
+Set-PSReadLineKeyHandler -Chord Ctrl+n -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord Ctrl+p -Function HistorySearchBackward
+Invoke-Expression (&starship init powershell)
 
-Set-Alias fcd fuzzy-cd
-
-Set-Alias l Get-ChildItemColor -Option AllScope
-
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
-
-Set-Alias ~ cuserprofile -Option AllScope
-
-Set-Alias doomrefresh "~/.emacs.d/bin/doom refresh"
-
-cls
